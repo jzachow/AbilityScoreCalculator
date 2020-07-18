b120fc1df5f583f79fbe9ff2d980e35f8060b6f2
Justin <justin.zachow@gmail.com>
2020-07-18 11:13:47 -0400

    Commit initial build of program

---
+++ AbilityScoreCalculator/AbilityScoreCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbilityScoreCalculator
+{
+    class AbilityScoreCalculator
+    {
+        public int Roll = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            //Divide the starting roll
+            double divided = Roll / DivideBy;
+
+            //Add to the result
+            int added = AddAmount + (int)divided;
+
+            //Make sure it's greater than the minimum
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+    }
+}
---
+++ AbilityScoreCalculator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace AbilityScoreCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+
+            while (true)
+            {
+                calculator.Roll = ReadInt(calculator.Roll, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+
+                Console.WriteLine($"Calculated ability score: {calculator.Score}");
+                Console.WriteLine("Press Q to quit, any other key to continue");
+
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+
+            }
+        }
+
+        /// <summary>
+        /// Writes a prompt and reads an int value from the console
+        /// </summary>
+        /// <param name="lastUsedValue">The default value</param>
+        /// <param name="prompt">Prompt to print to the console</param>
+        /// <returns>The int value read, or the default value if unable to parse</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.WriteLine($"{prompt}: {lastUsedValue}");
+            var userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int value))
+            {
+                Console.WriteLine($"Using value: {value}");
+                return value;
+            }
+            else
+            {
+                Console.WriteLine($"Using default value: {lastUsedValue}");
+                return lastUsedValue;
+            }
+        }
+
+        /// <summary>
+        /// Writes a prompt and reads a double value from the console
+        /// </summary>
+        /// <param name="lastUsedValue">The default value</param>
+        /// <param name="prompt">Prompt to print to the console</param>
+        /// <returns>the double value read, or the default value if unable to parse</returns>
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.WriteLine($"{prompt}: {lastUsedValue}");
+            var userInput = Console.ReadLine();
+
+            if (double.TryParse(userInput, out double value))
+            {
+                Console.WriteLine($"Using value: {value}");
+                return value;
+            }
+            else
+            {
+                Console.WriteLine($"Using default value: {lastUsedValue}");
+                return lastUsedValue;
+            }
+        }
+    }
+}
